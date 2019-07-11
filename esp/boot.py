a4f8924db79e09cfe163c33b0b569e35f9aaf3d6
Juan Rubio <juan.rubio@globant.com>
2019-07-10 23:13:05 -0500

    Micropython

---
+++ esp/boot.py
@@ -0,0 +1,65 @@
+import time
+import json
+import random
+import network
+from umqtt.simple import MQTTClient 
+from ntptime import settime
+
+CONFIG = {
+    "broker": "35.211.233.243",
+    "sensor_pin": 0, 
+    "client_id": b"esp8266_test",
+    "topic": "sensor_data",
+}
+
+
+def strTimeProp(start, end, format, prop):
+    stime = time.mktime(start)
+    etime = time.mktime(end)
+    ptime = stime + int(prop * (etime - stime))
+    return time.localtime(ptime)
+
+
+def randomDate(start, end, prop):
+    return strTimeProp(start, end, '%Y-%m-%d %H:%M', prop)
+
+def uniform(a, b):
+    return a + (b-a) * random.getrandbits(32)/2**32
+
+def uniform_int(a, b):
+    return round(a + (b-a) * random.getrandbits(32)/2**32)
+
+def rand_char():
+    return [chr(uniform_int(97, 97 + 25)), chr(uniform_int(48, 48 + 9))][uniform_int(0, 1)]
+
+def rand_str(l):
+    return ''.join([rand_char() for i in range(l)])
+
+def uuid4():
+    return '{}-{}-{}-{}-{}'.format(rand_str(8), rand_str(4), rand_str(4), rand_str(4), rand_str(8))
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+connected = wlan.isconnected()
+#wlan.connect('HUAWEIP20', 'c44173a456c3')
+#wlan.connect('Camu', 'intern3t')
+#wlan.connect('ADEJANDO', 'kiriku22')
+wlan.connect('Dantiteis', 'Standar.1406')
+while not connected:
+    print('connecting wifi')
+    connected = wlan.isconnected()
+    time.sleep(1)
+
+settime()
+
+client = MQTTClient(CONFIG['client_id'], CONFIG['broker'])
+client.connect()
+print("Connected to {}".format(CONFIG['broker']))
+
+#ts = time.time()
+points = [
+	(5.217041, -73.542104, "Rio Bogota Nacedero", "1"),
+	(5.150290, -73.690503, "Rio Bogota Choconta", "2"),
+	(4.750229, -74.127806, "Rio Bogota Siberia", "3"),
+	(-1.418765, -70.587582, "Rio Caqueta Zona Media", "4")
+]
---
+++ esp/main.py
@@ -0,0 +1,87 @@
+import machine
+import onewire, ds18x20
+
+temperature_pin = machine.Pin(2)
+ds = ds18x20.DS18X20(onewire.OneWire(temperature_pin))
+roms = ds.scan()
+
+sensor = machine.ADC(0)
+S1 = machine.Pin(16, machine.Pin.OUT)
+S2 = machine.Pin(5, machine.Pin.OUT)
+S3 = machine.Pin(4, machine.Pin.OUT)
+
+Trigger = machine.Pin(14, machine.Pin.OUT)
+Echo = machine.Pin(12, machine.Pin.IN)
+
+
+while True:
+    print("Creating new data point...")
+    #timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = randomDate((2019, 05, 30, 0, 0, 0, 0, 0), (2019, 05, 31, 12, 0, 0, 0, 0), random.getrandbits(32)/2**32)
+    event_uuid = uuid4()
+    point = points[random.getrandbits(2)]
+
+    ds.convert_temp() #temperature
+    temperature_level = ds.read_temp(roms[0])
+
+    S1.value(0)
+    S2.value(0)
+    S3.value(0)
+    ph_value = (1023-sensor.read())/73.07
+    time.sleep(1)
+
+    S1.value(1)
+    S2.value(0)
+    S3.value(0)
+    level_value = sensor.read()
+    time.sleep(1)
+    
+    Trigger.value(0)
+    Trigger.value(1)
+    time.sleep(10/1000000)
+    Trigger.value(0)
+    
+    while not Echo.value():
+    	pass
+    start = time.ticks_us()
+    while Echo.value():
+    	pass
+    width = time.ticks_diff(time.ticks_us(), start)
+    level_value2 = width/59
+
+    payload = {
+        'measure_id': event_uuid,
+        'device_id': point[3],
+        'description': point[2],
+        'lat': point[0],
+        'lng': point[1],
+        'event_timestamp': '{}-{}-{} {}:{}:{}'.format(timestamp[0], timestamp[1], timestamp[2], timestamp[3], timestamp[4], timestamp[5]),
+        'metrics': [
+            {
+                'name': 'temperature',
+                'value': temperature_level
+            },
+
+            {
+                'name': 'level',
+                'value': level_value
+            },
+            {
+                'name': 'ph',
+                'value': ph_value
+            },
+            {
+                'name': 'level_2',
+                'value': level_value2
+            }
+        ]
+    }
+    
+    print("Payload: {}".format(json.dumps(payload)))
+    print("Publishing message to topic", CONFIG['topic'])
+    try:    
+        client.publish(CONFIG['topic'], bytes(str(json.dumps(payload)), 'utf-8'))
+    except:
+        print('Could not publish measure')
+    time.sleep(1)
+
